c54cd14e38caca9839b002208b381c686e919d9b
jeddyn <zar1117@o2.pl>
2017-11-05 00:38:26 +0100

    Three out of ten points
    up

---
+++ src/main/java/com/zar/demo/zar/demo/controller/TweetController.java
@@ -0,0 +1,30 @@
+package com.zar.demo.zar.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Controller public class TweetController {
+    @Autowired
+    private Twitter twitter;
+
+    @RequestMapping("/")
+    public String hello(@RequestParam(defaultValue = "csgo") String search, Model model) {
+
+        SearchResults searchResults = twitter.searchOperations().search(search);
+        List<Tweet> tweets = searchResults.getTweets();
+        model.addAttribute("tweets", tweets);
+        model.addAttribute("search", search);
+    return "resultPage";}
+}
+
